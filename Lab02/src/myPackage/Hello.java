4c1bc57e94ff44e0e14b59ed78ab2d513471c177
jumanaabdullah <jumanaabdullah@localhost>
2025-02-26 22:27:38 -0800

    second commit

--- Lab02/src/myPackage/Hello.java
+++ Lab02/src/myPackage/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 	System.out.println("yang - This line was added by Dr. Yang.");
 	// Add a new line with your information
 	System.out.println("303218565- This line was added by Colin Nugent");
-	System.out.println("303069533 - This line was added by Jumana Abdullah");
+	System.out.println("303069533 - This line was added by Jumana Abdullah ");
 	System.out.println("SacStateID - This line was added by First name Last name.");
 	System.out.println("SacStateID - This line was added by First name Last name.");
 	System.out.println("SacStateID - This line was added by First name Lastname.");
